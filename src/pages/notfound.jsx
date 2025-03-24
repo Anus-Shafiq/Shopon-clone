eda19d93db51546a82572585ca034d5d59ae4d31
Anus Shafiq <sheikhanus925@gmail.com>
2025-03-24 23:29:51 +0500

    Update notfound.jsx

--- src/pages/notfound.jsx
+++ src/pages/notfound.jsx
@@ -2,7 +2,8 @@ export default function NotFound() {
   return (
     <>
       <div className="text-center">
-        <h3>Sorry ! The Page you're looking for not found</h3>
+        <h1>Sorry!</h1>
+        <h3>The Page you're looking for not found</h3>
 
         <h1>404</h1>
       </div>
